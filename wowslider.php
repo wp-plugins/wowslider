33f0d82f1ff22d80f71b17b567581bc853199df0
WOWSlider.com <WOWSlider.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-15 12:27:06 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/wowslider/trunk@966328 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wowslider.php
+++ wowslider.php
@@ -3,7 +3,7 @@
 Plugin Name:  WOW Slider
 Description: This module easily adds image sliders created with WOWSlider app.
 Author: WOWSlider.com
-Version: 5.6
+Version: 6.1
 Author URI: http://wowslider.com/
 */
 // template tag
